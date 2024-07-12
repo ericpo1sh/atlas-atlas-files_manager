0b6943a63f95c42cbea862c31370e314451e0e86
ericpo1sh <ericpo1sh@gmail.com>
2024-07-12 14:37:57 -0500

    saving changes, class is complete but not working

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.db = this.client.db(database);
+    (async () => {
+      try {
+          await this.client.connect();
+      } catch (error) {
+          console.error('Failed to connect to the database', error);
+      }
+    })();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.db.collection('users');
+    const count = await collection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    const count = await collection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
