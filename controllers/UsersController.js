b589687acb837934a57e8151449f80bd3e40ffee
ericpo1sh <ericpo1sh@gmail.com>
2024-07-16 16:13:41 -0500

    added routing, UsersController class with functions

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import dbClient from '../utils/db.js';
+import RedisClient from '../utils/redis';
 const sha1 = require('sha1')
+const mongo = require('mongodb')
 
 class UserController {
   static async postUsers(req, res) {
@@ -18,6 +20,21 @@ class UserController {
     const addedUser = { email, password: hashed_pw };
     const finishUser = await dbClient.client.db().collection('users').insertOne(addedUser);
     return res.status(201).json({ Id: finishUser.insertedId, email })
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token']
+    console.log(token);
+    const key = 'auth_' + token;
+    const userId = await RedisClient.get(key);
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+    // console.log(checkForKey + ' <--- this is the key')
+    const getUser = await dbClient.client.db().collection('users').findOne({
+      _id: new mongo.ObjectId(userId)
+    })
+    res.status(200).json({ id: getUser._id, email: getUser.email})
   }
 }
 
