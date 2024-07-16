17b80d787329585bb593e54bd3a2a85e37ab4216
ericpo1sh <ericpo1sh@gmail.com>
2024-07-16 11:28:03 -0500

    filled in server.js file and index.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const route = require('./routes/index');
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const port = 5000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use('/', route);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+module.exports = app;
