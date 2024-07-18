ec3ae5de9a15a2c88f7002daffef2b30d06d517c
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-07-17 20:27:34 -0500

    new routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController')
-const AuthController = require('../controllers/AuthController')
-const FilesController = require('../controllers/FilesController')
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router({ mergeParams: true });
 
@@ -12,6 +12,8 @@ router.post('/users', UsersController.postUsers);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload)
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 module.exports = router;
